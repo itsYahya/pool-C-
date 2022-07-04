38ee1c4e88567a03f0c16ec8445d5c20fbc1c046
Yahya El mrabti <yel-mrab@e1r4p11.1337.ma>
2022-07-04 14:05:36 +0100

    ex02

---
+++ CPP03/ex02/src/Fixed.cpp
@@ -0,0 +1,146 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/30 06:55:42 by yel-mrab          #+#    #+#             */
+/*   Updated: 2022/07/04 14:02:28 by yel-mrab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <cmath>
+
+Fixed::Fixed(){
+    std::cout << "Default constructor called\n";
+    fxpoint = 0;
+}
+
+Fixed::Fixed(Fixed const &fixed){
+    std::cout << "Copy constructor called\n";
+    fxpoint = fixed.fxpoint;
+}
+
+Fixed::~Fixed(){
+    std::cout << "Destructor called\n";
+}
+
+int Fixed::getRawBits(void){
+    std::cout << "getRawBits member function called\n";
+    return (this->fxpoint);
+}
+
+void    Fixed::setRawBits(int const raw){
+    std::cout << "setRawBits member function called\n";
+    this->fxpoint = raw;
+}
+
+Fixed::Fixed(int const num){
+    std::cout << "Int constructor called\n";
+    fxpoint = roundf(num * (1 << nfb));
+}
+
+Fixed::Fixed(float const num){
+    std::cout << "Float constructor called" << std::endl;
+    fxpoint = roundf(num * (1 << nfb));
+}
+
+float Fixed::toFloat(void) const{
+    return ((float) fxpoint / (float)(1 << nfb));
+}
+
+int Fixed::toInt(void) const{
+    return (fxpoint >> nfb);
+}
+
+Fixed   &Fixed::operator=(const Fixed &fixed){
+    std::cout << "Copy assignment operator calle\n";
+    this->fxpoint = fixed.fxpoint;
+    return (*this);
+}
+
+std::ostream & operator<<(std::ostream &out, const Fixed &fixed){
+    out << fixed.toFloat();
+    return (out);
+}
+
+Fixed   Fixed::operator+(const Fixed &fixed) const{
+    Fixed   value(this->toFloat() + fixed.toFloat());
+    return (value);
+}
+
+Fixed   Fixed::operator-(const Fixed &fixed) const{
+    Fixed   value(this->toFloat() - fixed.toFloat());
+    return (value);
+}
+
+Fixed   Fixed::operator*(const Fixed &fixed) const{
+    Fixed   value(this->toFloat() * fixed.toFloat());
+    return (value);
+}
+
+Fixed   Fixed::operator/(const Fixed &fixed) const{
+    Fixed   value(this->toFloat() / fixed.toFloat());
+    return (value);
+}
+
+bool    Fixed::operator>(const Fixed &fixed) const{
+    return (this->toFloat() > fixed.toFloat());
+}
+
+bool    Fixed::operator>=(const Fixed &fixed) const{
+    return (this->toFloat() >= fixed.toFloat());
+}
+
+bool    Fixed::operator<(const Fixed &fixed) const{
+    return (this->toFloat() < fixed.toFloat());
+}
+
+bool    Fixed::operator<=(const Fixed &fixed) const{
+    return (this->toFloat() <= fixed.toFloat());
+}
+
+bool    Fixed::operator==(const Fixed &fixed) const{
+    return (this->toFloat() == fixed.toFloat());
+}
+
+bool    Fixed::operator!=(const Fixed &fixed) const{
+    return (this->toFloat() != fixed.toFloat());
+}
+
+Fixed   &Fixed::min(Fixed &fixedL, Fixed &fixedR){
+    if (fixedL < fixedR)
+        return (fixedL);
+    return (fixedR);
+}
+
+Fixed  const &Fixed::min(const Fixed &fixedL, const Fixed &fixedR){
+    if (fixedL < fixedR)
+        return (fixedL);
+    return (fixedR);
+}
+
+Fixed   &Fixed::max(Fixed &fixedL, Fixed &fixedR){
+    if (fixedL > fixedR)
+        return (fixedL);
+    return (fixedR);
+}
+
+Fixed  const &Fixed::max(const Fixed &fixedL, const Fixed &fixedR){
+    if (fixedL > fixedR)
+        return (fixedL);
+    return (fixedR);
+}
+
+Fixed   Fixed::operator++(){
+    fxpoint++;
+    return (*this);
+}
+
+Fixed   Fixed::operator++(int){
+    Fixed   fixed(*this);
+    fxpoint++;
+    return (fixed);
+}
---
+++ CPP03/ex02/src/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 03:35:52 by yel-mrab          #+#    #+#             */
+/*   Updated: 2022/07/04 10:48:17 by yel-mrab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+int main( void )
+{
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+    return (0);
+}
