ba77485839bd33ef870429ca77bd5ff3a5bf8db3
Yahya El mrabti <yel-mrab@e3r3p15.1337.ma>
2022-08-05 03:28:02 +0100

    day04/ex02

---
+++ CPP05/ex02/inc/cat.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/04 15:58:57 by yel-mrab          #+#    #+#             */
+/*   Updated: 2022/08/05 01:42:39 by yel-mrab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CAT_HPP
+#define CAT_HPP
+
+#include "animal.hpp"
+#include "brain.hpp"
+
+class Cat : public Animal{
+	private :
+		Brain	*brain;
+		
+	public :
+		Cat();
+		Cat(Cat const &cat);
+		~Cat();
+		Cat		&operator=(Cat const &cat);
+		void	makeSound(void) const;
+		Brain	getBrain(void);
+};
+
+#endif
